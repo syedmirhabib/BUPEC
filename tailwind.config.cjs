06934f5ea6948d7b12685ab3e99a2bdd8c4d3907
Syed Mir Habib <mirakik.habib@gmail.com>
2023-03-28 11:04:39 +0600

    daisyUI randomize theme added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  
+  // tailwindcss content
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+
+  // daisyUI randomize theme
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#80b709",
+        
+"secondary": "#974ad6",
+        
+"accent": "#ea9707",
+        
+"neutral": "#181826",
+        
+"base-100": "#F1EBF4",
+        
+"info": "#386FC7",
+        
+"success": "#29C28F",
+        
+"warning": "#EFC406",
+        
+"error": "#EA3968",
+        },
+      },
+    ],
   },
+
   //daisyUI plugin
   plugins: [require("daisyui")],
 }
